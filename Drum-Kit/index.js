b43443e9d81d701eb807577a2dc3ad3423943f92
Yousef Ali <yousefabdelbadea@gmail.com>
2024-10-21 23:16:37 +0300

    Drum-Kit play audio when i press button

--- Drum-Kit/index.js
+++ Drum-Kit/index.js
@@ -1,8 +1,8 @@
 let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (let i = 0; i < numberOfDrumButtons; i++) 
-{
-document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked!");
-    });
+for (let i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    let audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+  }); 
 }
