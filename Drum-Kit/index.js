49b41b2a1be158dc5dd11dfb099ca919d470ef94
Yousef Ali <yousefabdelbadea@gmail.com>
2024-10-21 23:03:50 +0300

    Drum-Kit add Event Lisenter to all Buttons

---
+++ Drum-Kit/index.js
@@ -0,0 +1,8 @@
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) 
+{
+document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("I got clicked!");
+    });
+}
